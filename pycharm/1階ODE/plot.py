78afd8042899260735e856dcd25f9bfcf9300971
rubegu <orenzihariken4@gmail.com>
2022-02-19 22:08:15 +0900

    a

--- pycharm/1階ODE/plot.py
+++ pycharm/1階ODE/plot.py
@@ -8,19 +8,19 @@ y_runge4 = np.zeros(n)
 
 
 for index, reg_nbit in enumerate(reg_nbit_list):
-    with open("1階ODE数値結果/HHL(number=" + str(number) + ",reg_qubits=" + str(reg_nbit) + ",h=" + str(
+    with open("1階ODE数値結果/number=" + str(number) + "/HHL(number=" + str(number) + ",reg_qubits=" + str(reg_nbit) + ",h=" + str(
             h) + ",n=" + str(n) + ").csv", newline="") as f:
         reader = csv.reader(f)
         for i, row in enumerate(reader):
             HHL_runge_result[index][i] = row[1]
 
-with open("1階ODE数値結果/NEWTON_EXACT(number=" + str(number) + ",h=" + str(
+with open("1階ODE数値結果/number=" + str(number) + "/NEWTON_EXACT(number=" + str(number) + ",h=" + str(
         h) + ",n=" + str(n) + ").csv", newline="") as f:
     reader = csv.reader(f)
     for i, row in enumerate(reader):
         y_exact_newton[i] = row[1]
 
-with open("1階ODE数値結果/RUNGE_EXACT(number=" + str(number) + ",h=" + str(
+with open("1階ODE数値結果/number=" + str(number) + "/RUNGE_EXACT(number=" + str(number) + ",h=" + str(
         h) + ",n=" + str(n) + ").csv", newline="") as f:
     reader = csv.reader(f)
     for i, row in enumerate(reader):
@@ -45,7 +45,7 @@ plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', fontsize='large')
 
 plt.xlabel("x")
 plt.ylabel("y")
-fig.savefig("1階ODE数値結果/plot(number=" + str(number) + ",reg_qubits=" + str(reg_nbit_list) + ",h=" + str(
+fig.savefig("1階ODE数値結果/number=" + str(number) + "/plot(number=" + str(number) + ",reg_qubits=" + str(reg_nbit_list) + ",h=" + str(
     h) + ",n=" + str(n) + ").png", bbox_inches="tight")
 plt.show()
 
@@ -70,6 +70,6 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.yscale("log")
 fig.savefig(
-    "1階ODE数値結果/error(number=" + str(number) + ",reg_qubits=" + str(reg_nbit_list) + ",h=" + str(
+    "1階ODE数値結果/number=" + str(number) + "/error(number=" + str(number) + ",reg_qubits=" + str(reg_nbit_list) + ",h=" + str(
         h) + ",n=" + str(n) + ").png", bbox_inches="tight")
 plt.show()
